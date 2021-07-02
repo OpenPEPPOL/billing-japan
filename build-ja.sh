c0f131c10db03e5f247e4aa522f5d13d1d5ca109
Eito Katagiri <eitoball@gmail.com>
2021-07-02 16:53:07 +0900

    Create a proposal to translate documents to Japanese

---
+++ build-ja.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+PROJECT=$(dirname $(readlink -f "$0"))
+
+if [ -e $PROJECT/target-ja ]; then
+    docker run --rm -i -v $PROJECT:/src alpine:3.11 rm -rf /src/target-ja
+fi
+
+# Structure
+docker run --rm -i \
+    -v $PROJECT:/src \
+    -v $PROJECT/target-ja:/src/target \
+    -v $PROJECT/target-ja:/target \
+    difi/vefa-structure:0.7
+
+
+# Validator
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target-ja:/src/target anskaffelser/validator:2.1.0 build -x -t -n eu.peppol.postaward.v3.billing -a rules,guide -target target/validator /src
+
+
+# Generate adoc-files from rules
+
+# CEN-EN16931-CII
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/CEN-EN16931-CII.sch -q:tools/xquery/rules_asciidoc_cen.xquery -o:/target/CEN-EN16931-CII-GENERAL.sch.adoc
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/CEN-EN16931-CII.sch -q:tools/xquery/rules_asciidoc_cen_syntax.xquery -o:/target/CEN-EN16931-CII-SYNTAX.sch.adoc
+
+# CEN-EN16931-UBL
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/CEN-EN16931-UBL.sch -q:tools/xquery/rules_asciidoc_cen.xquery -o:/target/CEN-EN16931-UBL-GENERAL.sch.adoc
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/CEN-EN16931-UBL.sch -q:tools/xquery/rules_asciidoc_cen_syntax.xquery -o:/target/CEN-EN16931-UBL-SYNTAX.sch.adoc
+
+# PEPPOL-EN16931-UBL
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/PEPPOL-EN16931-UBL.sch -q:tools/xquery/rules_asciidoc_peppol.xquery -o:/target/PEPPOL-EN16931-UBL-GENERAL.sch.adoc
+#docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/PEPPOL-EN16931-UBL.sch -q:tools/xquery/rules_asciidoc_peppol_national.xquery -o:/target/PEPPOL-EN16931-UBL-NATIONAL.sch.adoc
+
+# PINT-UBL
+docker run --rm -i -v $PROJECT:/src -v $PROJECT/target-ja/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/PINT-UBL-validation-preprocessed.sch -q:tools/xquery/rules_asciidoc_peppol.xquery -o:/target/PINT-UBL-GENERAL.sch.adoc
+
+# Guides
+docker run --rm -i -v $PROJECT/guide-ja:/documents/guide -v $PROJECT/rules:/documents/rules -v $PROJECT/.postasciidoctor.sh:/documents/.postasciidoctor.sh -v $PROJECT/target-ja:/target difi/asciidoctor
+
+
+# Fix ownership
+docker run --rm -i -v $PROJECT:/src alpine:3.11 chown -R $(id -g $USER).$(id -g $USER) /src/target-ja
